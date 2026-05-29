a1fd68bc8bd480c7e95c214092f1ba9b2f29d937
Hardik01363 <hardikkhandelwal444@gmail.com>
2026-05-29 13:34:03 +0530

    include: completed disk manager header file

--- include/storage/disk_manager.h
+++ include/storage/disk_manager.h
@@ -1,9 +1,16 @@
 #pragma once
 
 #include "../config.h"
+#include <fstream>
+#include <mutex>
 
 namespace mnemos{
     class DiskManager {
+        private:
+            std::string open_file_name;
+            std::fstream open_file_handle;
+            std::mutex lock;
+
         public:
             DiskManager(std::string filename);
             bool readpg(page_id_t, std::byte* buffer_ptr);
