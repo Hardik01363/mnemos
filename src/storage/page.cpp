9be4c01b10eb624dd6be2e3b3ae92e4fa61b5b77
Hardik01363 <hardikkhandelwal444@gmail.com>
2026-06-02 17:28:12 +0530

    Fixed some typos

--- src/storage/page.cpp
+++ src/storage/page.cpp
@@ -1,4 +1,4 @@
-//include statements
+#include "../../include/storage/page.h"
 #include <cstring>
 #include <utility>
 
@@ -25,7 +25,7 @@ namespace mnemos {
 
     std::pair<bool, uint16_t> Page::read_slot(slot_id_t id, uint8_t* buffer) {
         uint16_t slot_cnt = get_slot_cnt();
-        if(id >= slot_cnt || *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + id * sizeof(SlotEntry)) == 0) return std::make_pair(false, 0);
+        if(id >= slot_cnt || *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + id * sizeof(SlotEntry)]) == 0) return std::make_pair(false, 0);
         
         uint16_t tuple_offset = *reinterpret_cast<uint16_t*>(PAGE_HEADER_SIZE + id * sizeof(SlotEntry));
         uint16_t read_data_length = *reinterpret_cast<uint16_t*>(PAGE_HEADER_SIZE + id * sizeof(SlotEntry) + 2);
@@ -36,21 +36,21 @@ namespace mnemos {
     bool Page::update_slot(slot_id_t id, uint8_t* input_data, uint16_t length) {
         uint16_t slot_cnt = get_slot_cnt();
 
-        if(id >= slot_cnt || *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + id * sizeof(SlotEntry)) == 0) return false;
+        if(id >= slot_cnt || *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + id * sizeof(SlotEntry)]) == 0) return false;
         
-        uint16_t old_offset = *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + id * sizeof(SlotEntry]));
+        uint16_t old_offset = *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + id * sizeof(SlotEntry)]);
         uint16_t old_length = *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + id * sizeof(SlotEntry) + 2]);
         
         if(length <= old_length) {
-            memcpy(&data[old_offset, input_data, length);
+            memcpy(&data[old_offset], input_data, length);
             *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + id * sizeof(SlotEntry) + 2]) = length;
             return true;
         }
         
         if(length > fsp - PAGE_HEADER_SIZE - slot_cnt * sizeof(SlotEntry)) return false;
 
-        *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + id * sizeof(SlotEntry])) = fsp - length;
-        *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + id * sizeof(SlotEntry]) + 2) = length;
+        *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + id * sizeof(SlotEntry)]) = fsp - length;
+        *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + id * sizeof(SlotEntry]) + 2]) = length;
         memcpy(&data[fsp - length], input_data, length);
         set_fsp(fsp - length);
         return true;
@@ -60,8 +60,8 @@ namespace mnemos {
         uint16_t slot_cnt = get_slot_cnt();
         if(id >= slot_cnt) return false;
         
-        *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + id * sizeof(SlotEntry])) = 0;
-        *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + id * sizeof(SlotEntry]) + 2) = 0;
+        *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + id * sizeof(SlotEntry)]) = 0;
+        *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + id * sizeof(SlotEntry) + 2]) = 0;
         return true;
     }
 
@@ -72,7 +72,7 @@ namespace mnemos {
         memcpy(&page[0], &data[0], PAGE_SIZE);
 
         for(int i = 0; i < slot_cnt; i++) {
-            uint16_t i_offset = *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + i * sizeof(SlotEntry]));
+            uint16_t i_offset = *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + i * sizeof(SlotEntry)]);
             uint16_t i_length = *reinterpret_cast<uint16_t*>(&data[PAGE_HEADER_SIZE + i * sizeof(SlotEntry) + 2]);
 
             if(i_offset == 0) continue;
